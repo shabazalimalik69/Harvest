e6efd497f1588fbcd02873d16948bb7fc0eecc57
Ankur982 <singh999ankur@gmail.com>
2022-09-28 18:40:29 +0530

    adding nabar

--- harvest_clone/src/App.js
+++ harvest_clone/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar1 from './Components/Navbar/Navbar1/Navbar1';
 import AllRoutes from './Routes/AllRoutes';
 import { Box } from '@chakra-ui/react';
+import { Footer } from './Components/Footer/Footer';
+
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
 
       <AllRoutes/>
       </Box>
-      
+     <Footer/>
     </div>
   );
 }
