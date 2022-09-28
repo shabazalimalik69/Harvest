a6a204d1199db85c2bb79600fc60c27604f61aa1
AkashKeshari111 <akashkeshari2722000@gmail.com>
2022-09-28 12:12:54 +0530

    responsive navbar1

--- harvest_clone/src/Components/Navbar/Navbar1/Navbar1.jsx
+++ harvest_clone/src/Components/Navbar/Navbar1/Navbar1.jsx
@@ -1,11 +1,23 @@
-import { Box, Button, Image, ListItem, Text, UnorderedList } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Image,
+  ListItem,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import style from "./Navbar1.module.css";
+import {
+  AiOutlineApple,
+  AiOutlineAndroid,
+  AiOutlineMenu,
+} from "react-icons/ai";
 
 const Navbar1 = () => {
   const [logo, setLogo] = useState(false);
-  const [show,hide]=useState(false)
+  const [show, hide] = useState(false);
 
   const changeBackground = () => {
     if (window.scrollY >= 80) {
@@ -17,90 +29,248 @@ const Navbar1 = () => {
 
   window.addEventListener("scroll", changeBackground);
 
+  const handleNav = () => {
+    hide(!show);
+  };
 
-  const handleNav=()=>{
-    hide(!show)
-  }
-  
+  const handleClose = () => {
+    setTimeout(() => {
+      hide(false);
+    }, 500);
+  };
+
   return (
     <Box>
-    <Box bgColor={logo?"white":"rgb(251,242,235)"} h="90px"  pt={1} position="fixed" w="100%" display="flex" alignItems="center" zIndex={1000} fontFamily="MuotoWeb, Helvetica Neue, Helvetica, Arial, sans-serif" fontWeight={500} className={style.div}>
-    
+      <Box
+        bgColor={logo ? "white" : "rgb(251,242,235)"}
+        h="90px"
+        pt={1}
+        position="fixed"
+        w="100%"
+        display="flex"
+        alignItems="center"
+        zIndex={1000}
+        fontFamily="MuotoWeb, Helvetica Neue, Helvetica, Arial, sans-serif"
+        fontWeight={500}
+        className={style.div}
+      >
+        <UnorderedList className={logo ? style.topnav1 : style.topnav}>
+          <ListItem>
+            <Link to="/harvest" className={style.active}>
+              <Image src="harvest_Logo.png" w="100%" mt={-5} />
+            </Link>
+          </ListItem>
 
-      <UnorderedList className={logo?style.topnav1:style.topnav}>
+          <ListItem className={style.display}>
+            <Link to="/whyHarvest">Why Harvest ?</Link>
+          </ListItem>
 
-        <ListItem>
-          <Link to="/harvest" className={style.active}>
-        <Image src="harvest_Logo.png" w="100%"    mt={-5}/>
-          </Link>
-        </ListItem>
-       
+          <ListItem className={style.display}>
+            <Link to="/features">Features</Link>
+          </ListItem>
 
-        <ListItem className={style.display}>
-          <Link to="/whyHarvest">Why Harvest?</Link>
-        </ListItem>
+          <ListItem className={style.display}>
+            <Link to="/customers">Customers</Link>
+          </ListItem>
 
-        <ListItem className={style.display}>
-          <Link to="/features">Features</Link>
-        </ListItem>
+          <ListItem className={style.display}>
+            <Link to="intregations">Intregations</Link>
+          </ListItem>
 
-        <ListItem className={style.display}>
-          <Link to="/customers">Customers</Link>
-        </ListItem>
+          <ListItem className={style.display}>
+            <Link to="pricing">Pricing</Link>
+          </ListItem>
 
-        <ListItem className={style.display}>
-          <Link to="intregations">Intregetions</Link>
-        </ListItem>
+          <ListItem className={style.display1}>
+            <Text
+              onClick={handleNav}
+              w="100%"
+              display="flex"
+              alignItems="center"
+              justifyContent="flex-end"
+            >
+              <AiOutlineMenu />
+              <Text>Menu</Text>
+            </Text>
+          </ListItem>
 
-        <ListItem className={style.display}>
-          <Link to="pricing">Pricing</Link>
-        </ListItem>
-
-        <ListItem className={style.display1}>
-          <button onClick={handleNav}>ab</button>
-        </ListItem>
-
-        <ListItem className={style.right} >
-            <Box w="100%" h="100%" display="flex" alignItems="center" paddingTop={1} >
-            <Button borderRadius={10} bgColor="rgb(250,93,0)" fontSize="18px" paddingTop={1.5} paddingBottom={1.5} color="white" _hover={{outline:"rgb(250,93,0)",bgColor:"rgb(250,93,0)"}} _focus={{outline:"rgb(250,93,0)",bgColor:"rgb(250,93,0)"}}>Try Harvest Free</Button>
+          <ListItem className={style.right}>
+            <Box
+              w="100%"
+              h="100%"
+              display="flex"
+              alignItems="center"
+              paddingTop={1}
+            >
+              <Button
+                borderRadius={10}
+                bgColor="rgb(250,93,0)"
+                fontSize="18px"
+                paddingTop={1.5}
+                paddingBottom={1.5}
+                color="white"
+                _hover={{ outline: "rgb(250,93,0)", bgColor: "rgb(250,93,0)" }}
+                _focus={{ outline: "rgb(250,93,0)", bgColor: "rgb(250,93,0)" }}
+              >
+                Try Harvest Free
+              </Button>
             </Box>
-          
-        </ListItem>
-        <ListItem className={style.right}>
-          <Link to="/signin">Sign in</Link>
-        </ListItem>
+          </ListItem>
+          <ListItem className={style.right}>
+            <Link to="/signin">Sign in</Link>
+          </ListItem>
+        </UnorderedList>
+      </Box>
 
-      
-        
+      <Box className={show ? style.box1 : style.box2}>
+        <Box h="75%">
+          <Box className={style.box3}>
+            <Box className={style.box31}>
+              {" "}
+              <Link to="/whyHarvest">
+                <Box onClick={handleClose}>Why Harvest ?</Box>
+              </Link>
+            </Box>
+            <Box className={style.box31}>
+              {" "}
+              <Link to="/features">
+                <Box onClick={handleClose}>Features</Box>
+              </Link>
+            </Box>
+            <Box className={style.box31}>
+              {" "}
+              <Link to="/customers">
+                <Box onClick={handleClose}>Customers</Box>
+              </Link>
+            </Box>
+            <Box className={style.box31}>
+              {" "}
+              <Link to="/intregations">
+                <Box onClick={handleClose}>Intregations</Box>
+              </Link>
+            </Box>
+            <Box className={style.box31}>
+              {" "}
+              <Link to="/pricing">
+                <Box onClick={handleClose}>Pricing</Box>
+              </Link>
+            </Box>
+          </Box>
 
-      </UnorderedList>
-   
-    </Box>
-
-    <Box className={show?style.box1:style.box2} >
-        <Box>
-            <Box>Why Harvest?</Box>
-            <Box>Features</Box>
-            <Box>Customers</Box>
-            <Box>Intregetions</Box>
-            <Box>Pricing</Box>
+          <Box className={style.box4}>
+            <Box className={style.box41}>
+              {" "}
+              <Button
+                w="100%"
+                h="100%"
+                borderRadius={8}
+                bgColor="rgb(65, 65, 65)"
+                _hover={{
+                  outline: "rgb(65, 65, 65)",
+                  bgColor: "rgb(65, 65, 65)",
+                }}
+                _focus={{
+                  outline: "rgb(65, 65, 65)",
+                  bgColor: "rgb(65, 65, 65)",
+                }}
+              >
+                <Box>Sign in</Box>
+              </Button>{" "}
+            </Box>
+            <Box className={style.box41}>
+              {" "}
+              <Button
+                w="100%"
+                h="100%"
+                borderRadius={8}
+                bgColor="rgb(65, 65, 65)"
+                _hover={{
+                  outline: "rgb(65, 65, 65)",
+                  bgColor: "rgb(65, 65, 65)",
+                }}
+                _focus={{
+                  outline: "rgb(65, 65, 65)",
+                  bgColor: "rgb(65, 65, 65)",
+                }}
+              >
+                <Box>Try Harvest Free</Box>
+              </Button>
+            </Box>
+          </Box>
         </Box>
 
-        <Box>
-            <Box>Sign in</Box>
-            <Box>Try Harvest Free</Box>
+        <Box h="11%" className={style.b1}>
+          <Text fontWeight={500} padding={2}>
+            Get the Mobile App
+          </Text>
 
+          <Box className={style.box5}>
+            <Box className={style.box51}>
+              {" "}
+              <a
+                href="https://apps.apple.com/us/app/harvest-time-expense-tracker/id355395846"
+                target="_blank"
+              >
+                {" "}
+                <Button
+                  w="100%"
+                  h="100%"
+                  borderRadius={8}
+                  bgColor="rgb(65, 65, 65)"
+                  _hover={{
+                    outline: "rgb(65, 65, 65)",
+                    bgColor: "rgb(65, 65, 65)",
+                  }}
+                  _focus={{
+                    outline: "rgb(65, 65, 65)",
+                    bgColor: "rgb(65, 65, 65)",
+                  }}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <AiOutlineApple fontSize="20px" />
+                    <Text pl={1}>IPhone</Text>
+                  </Box>
+                </Button>
+              </a>{" "}
+            </Box>
+            <Box className={style.box51}>
+              {" "}
+              <a
+                href="https://play.google.com/store/apps/details?id=com.harvestapp"
+                target="_blank"
+              >
+                <Button
+                  w="100%"
+                  h="100%"
+                  borderRadius={8}
+                  bgColor="rgb(65, 65, 65)"
+                  _hover={{
+                    outline: "rgb(65, 65, 65)",
+                    bgColor: "rgb(65, 65, 65)",
+                  }}
+                  _focus={{
+                    outline: "rgb(65, 65, 65)",
+                    bgColor: "rgb(65, 65, 65)",
+                  }}
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <AiOutlineAndroid fontSize="20px" />
+                    <Text pl={1}>Andriod</Text>
+                  </Box>
+                </Button>
+              </a>
+            </Box>
+          </Box>
         </Box>
-
-        <Box>
-        <Box>
-            <Box>Sign in</Box>
-            <Box>Try Harvest Free</Box>
-
-        </Box>
-        </Box>
-
-    </Box>
+      </Box>
     </Box>
   );
 };
