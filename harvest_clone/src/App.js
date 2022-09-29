85d75b4d18f33b275cfcbf05e4054c6b0bfb5b59
AkashKeshari111 <akashkeshari2722000@gmail.com>
2022-09-29 11:12:55 +0530

    Navbar 2

--- harvest_clone/src/App.js
+++ harvest_clone/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-<<<<<<< HEAD
-import Feature from './Pages/Feature/Feature';
-=======
 import Navbar1 from './Components/Navbar/Navbar1/Navbar1';
 import AllRoutes from './Routes/AllRoutes';
 import { Box } from '@chakra-ui/react';
->>>>>>> a2d1577022c0c492c55d2da135b5b7e0e31d2c35
+import Navbar2 from './Components/Navbar/Navbar2/Navbar2';
+import { useState } from 'react';
 
 function App() {
+  const [token,setToken]=useState(false)
   return (
     <div className="App">
-<<<<<<< HEAD
-    <Feature/>
-=======
-      <Navbar1/>
+
+      {token?
+      <Box>
+      <Navbar2 setToken={setToken}/>
+      </Box>
+      :
+      <Box>
+      <Navbar1 setToken={setToken}/>
       <Box pt="90px">
 
       <AllRoutes/>
       </Box>
-      
->>>>>>> a2d1577022c0c492c55d2da135b5b7e0e31d2c35
+      </Box>}
     </div>
   );
 }
