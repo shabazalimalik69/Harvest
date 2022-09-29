9d532e0d4b6026988f5bda4fa6a7905994d022dc
shabaz ali malik <shabazalimalik69@gmail.com>
2022-09-29 21:54:23 +0530

    Signup Page

--- harvest_clone/src/Routes/AllRoutes.jsx
+++ harvest_clone/src/Routes/AllRoutes.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Pricing from "../Pages/Pricing/Pricing";
-
+import Feature from "../Pages/Feature/Feature"
+import Signup from "../Pages/Signup/Signup";
+import Signin from "../Pages/Signin/Signin";
 
 const AllRoutes = () => {
   return (
@@ -9,14 +11,15 @@ const AllRoutes = () => {
       <Route path="/harvest" element={<h1>Harvest</h1>} />
       <Route path="/whyHarvest" element={<h1>whyHarvest</h1>} />
 
-      <Route path="/features" element={<h1>Features</h1>} />
+      <Route path="/features" element={<Feature/>} />
 
       <Route path="/customers" element={<h1>Customers</h1>} />
 
       <Route path="/intregations" element={<h1>Intregations</h1>} />
       <Route path="/pricing" element={<Pricing/>} />
 
-      <Route path="/signin" element={<h1>signin</h1>} />
+      <Route path="/signin" element={<Signin/>} />
+      <Route path="/signup" element={<Signup/>} />
     </Routes>
   );
 };
