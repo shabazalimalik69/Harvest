23f88e38f0c3c16c00ee6a975a14395a823d2133
sombir5 <sombirverma5@gmail.com>
2022-09-29 09:53:48 +0530

    customer path =>pricing to customers

--- harvest_clone/src/Routes/AllRoutes.jsx
+++ harvest_clone/src/Routes/AllRoutes.jsx
@@ -1,4 +1,3 @@
-
 import Customers from "../Pages/Customers/Customers";
 
 import { Footer } from "../Components/Footer/Footer";
@@ -7,40 +6,46 @@ import Feature from "../Pages/Features/Features";
 
 import Pricing from "../Pages/Pricing/Pricing";
 
-
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/harvest" element={<h1>Harvest</h1>} />
       <Route path="/whyHarvest" element={<h1>whyHarvest</h1>} />
 
-      <Route path="/features" element={
-      <>
-      <Navbar1/>
-      <Feature/>
-      <Footer/>
-      </>
-    } />
+      <Route
+        path="/features"
+        element={
+          <>
+            <Navbar1 />
+            <Feature />
+            <Footer />
+          </>
+        }
+      />
 
-
- <Route path="/pricing" element={
-      <>
-      <Navbar1/>
-      <Customers/>
-      <Footer/>
-      </>
-    } />
+      <Route
+        path="/customers"
+        element={
+          <>
+            <Navbar1 />
+            <Customers />
+            <Footer />
+          </>
+        }
+      />
       <Route path="/intregations" element={<h1>Intregations</h1>} />
-      <Route path="/pricing" element={
-      <>
-      <Navbar1/>
-      <Pricing/>
-      <Footer/>
-      </>
-    } />
-    
-      <Route path="/pricing" element={<Pricing/>} />
+      <Route
+        path="/pricing"
+        element={
+          <>
+            <Navbar1 />
+            <Pricing />
+            <Footer />
+          </>
+        }
+      />
 
+      <Route path="/pricing" element={<Pricing />} />
 
       <Route path="/signin" element={<h1>signin</h1>} />
     </Routes>
