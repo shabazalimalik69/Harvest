42a94d6338aa8e3f6051abccab5c08a57c270d64
Ankur982 <singh999ankur@gmail.com>
2022-10-01 05:14:35 +0530

    resolving navbar error

--- harvest_clone/src/Routes/AllRoutes.jsx
+++ harvest_clone/src/Routes/AllRoutes.jsx
@@ -14,15 +14,15 @@ import { Footer } from "../Components/Footer/Footer";
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<><Home/> <Footer/></>} />
-      <Route path="/whyHarvest" element={<><Why_harvest /><Footer/></>} />
+      <Route path="/" element={<Home/>} />
+      <Route path="/whyHarvest" element={<Why_harvest />} />
 
-      <Route path="/features" element={<><Feature /><Footer/></>} />
+      <Route path="/features" element={<Feature />} />
 
-      <Route path="/customers" element={<><Customers /><Footer/></>} />
+      <Route path="/customers" element={<Customers />} />
 
-      <Route path="/intregations" element={<><Intregations/><Footer/></>} />
-      <Route path="/pricing" element={<><Pricing /><Footer/></>} />
+      <Route path="/intregations" element={<Intregations/>} />
+      <Route path="/pricing" element={<Pricing />} />
       <Route path="/signin" element={<Signin/>} />
       <Route path="/signup" element={<Signup/>} />
 
