cadea1c1f23a3f3e5bf3f2e26dfdf62c45d0af8b
sombir5 <sombirverma5@gmail.com>
2022-10-01 13:30:15 +0530

    expanse- CURD

--- harvest_clone/src/Store/expanse/expanse.reducer.js
+++ harvest_clone/src/Store/expanse/expanse.reducer.js
@@ -1,7 +1,50 @@
-import React from "react";
+import {
+  ADD_PROJECT,
+  GET_PROJECT,
+  EDIT_PROJECT,
+  DELETE_PROJECT,
+  ALL_DELETE_PROJECT,
+} from "./expanse.type";
 
-const expanseReducer = () => {
-  return <div>expanse.reducer</div>;
+const initialState = {
+  data: [],
+  page: 1,
+};
+const expanseReducer = (state = initialState, { type, payload }) => {
+  //   console.log(payload);
+  switch (type) {
+    case GET_PROJECT: {
+      return {
+        ...state,
+        data: payload,
+      };
+    }
+    case ADD_PROJECT: {
+      return {
+        ...state,
+        data: [...state.data, payload],
+      };
+    }
+    case EDIT_PROJECT: {
+      return {
+        ...state,
+        data: [...state.data, payload],
+      };
+    }
+    case DELETE_PROJECT: {
+      return {
+        ...state,
+      };
+    }
+    case ALL_DELETE_PROJECT: {
+      return {
+        ...state,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
 };
 
 export default expanseReducer;
